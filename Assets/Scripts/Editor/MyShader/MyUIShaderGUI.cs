5dec9e8b73f1bbf5f1342795818f0cf68d4db4b5
胡胜同学 <husheng.student@foxmail.com>
2020-03-02 22:29:58 +0800

    update ShaderGUI

--- Assets/Scripts/Editor/MyShader/MyUIShaderGUI.cs
+++ Assets/Scripts/Editor/MyShader/MyUIShaderGUI.cs
@@ -6,25 +6,44 @@ public class MyUIShaderGUI : ShaderGUI
 {
     public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
     {
-        base.OnGUI(materialEditor, properties);
-
         Material targetMat = materialEditor.target as Material;
 
-        bool MyTestBool = Array.IndexOf(targetMat.shaderKeywords, "MyTestBool") != -1;
+        ShaderProperty(materialEditor, properties, "_Color");
+        ShaderProperty(materialEditor, properties, "_StencilComp");
+        ShaderProperty(materialEditor, properties, "_Stencil");
+        ShaderProperty(materialEditor, properties, "_StencilOp");
+        ShaderProperty(materialEditor, properties, "_StencilWriteMask");
+        ShaderProperty(materialEditor, properties, "_StencilReadMask");
+        ShaderProperty(materialEditor, properties, "_ColorMask");
+        ShaderProperty(materialEditor, properties, "_UseUIAlphaClip");
+        materialEditor.RenderQueueField();
+        materialEditor.DoubleSidedGIField();
 
-        EditorGUI.BeginChangeCheck();
-        MyTestBool = EditorGUILayout.Toggle("MyTestBool", MyTestBool);
-
-        if (EditorGUI.EndChangeCheck())
         {
-            if (MyTestBool)
+            bool toggle = Array.IndexOf(targetMat.shaderKeywords, "DOODLE_ON") != -1;
+            toggle = EditorGUILayout.BeginToggleGroup("DOODLE_ON", toggle);
+            if (toggle)
             {
-                targetMat.EnableKeyword("MyTestBool");
+                targetMat.EnableKeyword("DOODLE_ON");
+                EditorGUILayout.BeginVertical();
+                {
+                    GUILayout.Space(10);
+                    EditorGUILayout.HelpBox("DOODLE_ON", MessageType.Info);
+                    ShaderProperty(materialEditor, properties, "_HandDrawnAmount");
+                    ShaderProperty(materialEditor, properties, "_HandDrawnSpeed");
+                }
+                EditorGUILayout.EndVertical();
             }
             else
             {
-                targetMat.DisableKeyword("MyTestBool");
+                targetMat.DisableKeyword("DOODLE_ON");
             }
         }
+    }
+
+    public void ShaderProperty(MaterialEditor materialEditor, MaterialProperty[] properties, string propertyName)
+    {
+        var property = FindProperty(propertyName, properties);
+        materialEditor.ShaderProperty(property, property.displayName);
     }
 }
