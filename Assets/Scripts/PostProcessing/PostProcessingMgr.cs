eaa5204707403738200548afdc2a2455ce39be2d
胡胜同学 <husheng.student@foxmail.com>
2020-03-09 22:42:17 +0800

    add PostProcessingMgr

---
+++ Assets/Scripts/PostProcessing/PostProcessingMgr.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework
+{
+    public class PostProcessingMgr : MonoSingleton<PostProcessingMgr>
+    {
+
+    }
+}
---
+++ Assets/Scripts/Shader/ShaderIDs.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Framework
+{
+    static class ShaderIDs
+    {
+        internal static readonly int MainTex = Shader.PropertyToID("_MainTex");
+
+    }
+}
