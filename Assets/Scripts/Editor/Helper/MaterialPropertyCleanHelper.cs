152b56b695bcc1740922d97bafec6f6d8afdced8
胡胜同学 <husheng.student@foxmail.com>
2020-03-22 22:43:50 +0800

    add property cleaner

---
+++ Assets/Scripts/Editor/Helper/MaterialPropertyCleanHelper.cs
@@ -0,0 +1,51 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class MaterialPropertyCleanHelper
+{
+    [MenuItem("Assets/myShaderLibrary/Helper/Clean MaterialProperty", false, 1)]
+    public static void MaterialPropertyClean()
+    {
+        var materials = UnityEditorHelper.GetSelectObjects("mat");
+        if (materials.Length > 0)
+        {
+            for (int i = 0; i < materials.Length; i++)
+            {
+                var mat = materials[i] as Material;
+                if (!mat)
+                {
+                    continue;
+                }
+
+                SerializedObject serializedObject = new SerializedObject(mat);
+                SerializedProperty savedProperties = serializedObject.FindProperty("m_SavedProperties");
+                SerializedProperty texEnvs = savedProperties.FindPropertyRelative("m_TexEnvs");
+                SerializedProperty floats = savedProperties.FindPropertyRelative("m_Floats");
+                SerializedProperty colors = savedProperties.FindPropertyRelative("m_Colors");
+
+                if (CleanSerializedProperty(texEnvs, mat) || CleanSerializedProperty(floats, mat)
+                    || CleanSerializedProperty(colors, mat))
+                {
+                    serializedObject.ApplyModifiedProperties();
+                    EditorUtility.SetDirty(mat);
+                }
+            }
+        }
+    }
+
+    private static bool CleanSerializedProperty(SerializedProperty property, Material mat)
+    {
+        bool dirty = false;
+        for (int i = property.arraySize - 1; i >= 0; i--)
+        {
+            var targetProperty = property.GetArrayElementAtIndex(i);
+            string propertyName = targetProperty.displayName;
+            if (!mat.HasProperty(propertyName))
+            {
+                property.DeleteArrayElementAtIndex(i);
+                dirty = true;
+            }
+        }
+        return dirty;
+    }
+}
---
+++ Assets/Scripts/Editor/Helper/UnityEditorHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class UnityEditorHelper
+{
+    public static Object[] GetSelectObjects(string extension)
+    {
+        var objectList = new List<Object>();
+        var objects = Selection.objects;
+        if (objects.Length > 0)
+        {
+            var pathList = new List<string>();
+            for (int i = 0; i < objects.Length; i++)
+            {
+                var target = objects[i];
+                var path = AssetDatabase.GetAssetPath(target);
+                if (target as DefaultAsset)
+                {
+                    pathList.Add(path);
+                }
+                else if (Path.GetExtension(path) == $".{extension}")
+                {
+                    objectList.Add(target);
+                }
+            }
+            foreach (var path in pathList)
+            {
+                string[] allFile = Directory.GetFiles(path, $"*.{extension}", SearchOption.AllDirectories);
+                foreach (var filePath in allFile)
+                {
+                    var temp = filePath.Replace("\\", "/");
+                    objectList.Add(AssetDatabase.LoadAssetAtPath(filePath, typeof(Object)));
+                }
+            }
+        }
+        return objectList.ToArray();
+    }
+}
