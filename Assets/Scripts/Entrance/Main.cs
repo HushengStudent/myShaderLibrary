1d8443a80e83fa5261f7a461dd01d9c6225e3817
胡胜同学 <husheng.student@foxmail.com>
2020-04-05 19:26:21 +0800

    add fps

--- Assets/Scripts/Entrance/Main.cs
+++ Assets/Scripts/Entrance/Main.cs
@@ -11,23 +11,57 @@ public class Main : MonoBehaviour
     private bool _isGreyScale = false;
     private bool _isGlitch = false;
 
+    private readonly float _fpsUpdateInterval = 0.5f;
+    private float _fps = 0;
+    private int _frames = 0;
+    private float _accumulator;
+    private float _timeLeft;
+
     private void Awake()
     {
         PostProcessMgr.singleton.Launch();
     }
 
+    private void Update()
+    {
+        _frames++;
+        _accumulator += Time.unscaledDeltaTime;
+        _timeLeft -= Time.unscaledDeltaTime;
+
+        if (_timeLeft <= 0f)
+        {
+            _fps = _accumulator > 0f ? _frames / _accumulator : 0f;
+            _frames = 0;
+            _accumulator = 0f;
+            _timeLeft += _fpsUpdateInterval;
+        }
+    }
+
     private void OnGUI()
     {
-        var w = GUILayout.Width(300);
-        var h = GUILayout.Height(100);
-        GUIStyle style = new GUIStyle(GUI.skin.button)
+        var w = GUILayout.Width(250);
+        var h = GUILayout.Height(75);
+        GUIStyle btnStyle = new GUIStyle(GUI.skin.button)
         {
-            fontSize = 30
+            fontSize = 30,
         };
-        var color = GUI.backgroundColor;
-        GUILayout.BeginHorizontal();
+        GUIStyle redTextStyle = new GUIStyle()
+        {
+            fontSize = 30,
+        };
+        redTextStyle.normal.textColor = Color.red;
 
-        if (GUILayout.Button(_isBlur ? "关闭模糊" : "开启模糊", style, w, h))
+        var backgroundColor = GUI.backgroundColor;
+        var color = GUI.color;
+        var contentColor = GUI.contentColor;
+
+        GUILayout.BeginVertical();
+
+        GUILayout.Label($" FPS：{(int)_fps}", redTextStyle);
+        GUILayout.Space(10);
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button(_isBlur ? "关闭模糊" : "开启模糊", btnStyle, w, h))
         {
             if (_isBlur)
             {
@@ -40,7 +74,7 @@ public class Main : MonoBehaviour
             _isBlur = !_isBlur;
         }
 
-        if (GUILayout.Button(_isGreyScale ? "关闭置灰" : "开启置灰", style, w, h))
+        if (GUILayout.Button(_isGreyScale ? "关闭置灰" : "开启置灰", btnStyle, w, h))
         {
             if (_isGreyScale)
             {
@@ -53,7 +87,7 @@ public class Main : MonoBehaviour
             _isGreyScale = !_isGreyScale;
         }
 
-        if (GUILayout.Button(_isGlitch ? "关闭故障" : "开启故障", style, w, h))
+        if (GUILayout.Button(_isGlitch ? "关闭故障" : "开启故障", btnStyle, w, h))
         {
             if (_isGlitch)
             {
@@ -67,5 +101,6 @@ public class Main : MonoBehaviour
         }
 
         GUILayout.EndHorizontal();
+        GUILayout.EndVertical();
     }
 }
